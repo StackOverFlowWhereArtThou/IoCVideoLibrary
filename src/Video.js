b99b597c3fbc7b17310d03247ec56b34a5cf3d19
Harry Fox <hfox@protonmail.com>
2021-06-17 10:33:01 -0500

    Updated Video and VR classes

--- src/Video.js
+++ src/Video.js
@@ -1,5 +1,8 @@
+// Class for a video to be stored
+
 class Video{
     constructor(videoData){
+        // video data should be an object including the video ID, title, publication year, and actual footage
         this.videoData = videoData;
     }
 }
